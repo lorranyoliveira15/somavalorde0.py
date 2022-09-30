ae8cae9296c6d8977cc531f1c62db87de57c03ff
lorranyoliveira15 <113057224+lorranyoliveira15@users.noreply.github.com>
2022-09-30 07:50:41 -0300

    Add files via upload

---
+++ somavalorde0.py
@@ -0,0 +1,11 @@
+soma:int
+x:int
+
+x = int(input("digite um número (0 - SAIR: "))
+soma = 0
+
+while  x != 0:
+    soma = soma + x
+    X = int(input("digite o número (0 - SAIR: "))
+
+print("soma vale",soma)
